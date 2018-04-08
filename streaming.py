7a6dfd2029bef3abd49fbb51b6690cc191a16925
Shubh0501 <shubham050197@gmail.com>
2018-04-08 22:40:14 +0530

    #Data

--- streaming.py
+++ streaming.py
@@ -4,10 +4,10 @@ import csv
 import urllib
 
 # Authentication details. 
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
+consumer_key = "LCah7GfN6Jncjak2skDX4xYY5"
+consumer_secret = "A2N8QOlfcY7VIkTYJCMAMzz3VnKhwDGW40T6UaScutqBSCLmXh"
+access_token = "873157993566810112-4qGuQQZf8Toz9fkNYogJHk2wvwHSzY5"
+access_token_secret = "Lkb92LyjlwPkAY8vsa4TmwbuZj4TWZLwzb2Fx6JYWSPoo"
 
 accountvar = raw_input("Enter the hashtag to be searched : ")  #Search query goes here
 
